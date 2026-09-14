96fe6d02b8082246e1ab53290412b676ecef5caa
dip9564 <dipmondal9564@gmail.com>
2026-09-14 18:22:09 +0530

    corsmiddleware

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="PDF Editor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["https://pdf-edit-rosy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
